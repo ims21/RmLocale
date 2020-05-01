676c6476341a5bb1ce732f080d6173f6917a1ffe
ims <ims21@users.sourceforge.net>
2020-05-01 09:56:33 +0200

    remove whitespaces

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from distutils.core import setup
 
 pkg = 'Extensions.RmLocale'
 setup (name = 'enigma2-plugin-extensions-rmlocale',
-       version = '1.05',
-       description = 'plugin for remove unused language files',
-       packages = [pkg],
-       package_dir = {pkg: 'plugin'},
-      )
+	version = '1.05',
+	description = 'plugin for remove unused language files',
+	packages = [pkg],
+	package_dir = {pkg: 'plugin'},
+	)
